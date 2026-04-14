9a3a78419830c6a165db5639f2b6ac6b87e9ea0f
Carlos Baylon <cbaylon2009@gmail.com>
2026-04-13 23:26:30 -0500

    commit -m "feat: update index.html, index.css and index.jsx for new features and bug fixes"

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import App from "./App";
 import { BrowserRouter } from 'react-router-dom';
-App.use(express.static(path.join(__dirname, "dist")));
 createRoot(document.getElementById('root')).render(<BrowserRouter>
     <App />
   </BrowserRouter>)
